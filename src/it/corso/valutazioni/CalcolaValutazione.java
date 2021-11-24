552c5de5bdb9df4f929fdc2bdbdc8ca7284d19e8
luisabattajni <luisasara.battajni@gmail.com>
2021-11-24 18:21:35 +0100

    minor edits - deleted most commented lines

--- src/it/corso/valutazioni/CalcolaValutazione.java
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -2,34 +2,12 @@ package it.corso.valutazioni;
 
 public class CalcolaValutazione {
 	
-	/*
-	 * tre attributi: 
-	 * id dello studente (intero), 
-	 * percentuale assenze (intero), 
-	 * media voti (numero decimale). 
-	 * La classe deve avere anche un metodo che calcola, 
-	 * in base a percentuale assenze e media voti, 
-	 * se lo studente è promosso o bocciato
-	 */
-	
-	/*
-	 * delete later
-	 * 
-	 * La valutazione complessiva viene fatta sulla percentuale di assenze (da 0 a 100) e la media dei voti (da 0 a 5):
-	 * se lo studente ha fatto più del 50% di assenze è bocciato
-	 * se ha fatto tra il 25% e il 50% di assenze è promosso solo se la media dei voti è superiore a 2
-	 * se ha fatto meno del 25% di assenze è promosso se la media dei voti è superiore o uguale a 2
-	 */
-	
-
-	
 	// attributi
 	int idStudente;
 	int percentualeAssenze;
 	double mediaVoti;
 	
 	// costruttori
-	//string?
 	
 	 CalcolaValutazione(int idStudente, int percentualeAssenze, double mediaVoti){
 	 this.idStudente = idStudente; 
@@ -39,16 +17,12 @@ public class CalcolaValutazione {
 	
 	//metodo
 	
-	
 	boolean promosso () {
 		if ((percentualeAssenze >= 25 && percentualeAssenze <= 50 && mediaVoti > 2) || (percentualeAssenze < 25 && mediaVoti >= 2)) {
-			
 			return true;
 		} else {
-			
 			return false;
 		}
-		
 	}
 
 	
--- src/it/corso/valutazioni/Main.java
+++ src/it/corso/valutazioni/Main.java
@@ -6,36 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * create un array di 20 CalcolaValutazione, 
-		 * lo popolate con id studente progressivo (da 1 a 20), 
-		 * percentuale assenze e media voti generati random. 
-		 * Per ogni elemento dell’array stampate a video 
-		 * l’id studente e se è promosso o bocciato
-		 */
-		
-		
 		Random randomGenerator = new Random();
-
-		
 		CalcolaValutazione[] randomValutazione = new CalcolaValutazione[20];
 		
 		int idStudente;
 		int percentualeAssenze;
 		double mediaVoti;
 
-
-		
 		for(int i = 0; i < randomValutazione.length; i++) {
 			percentualeAssenze = randomGenerator.nextInt(101);
 			mediaVoti = 5 * randomGenerator.nextDouble();
-			
 			idStudente = i + 1;
 			
 			randomValutazione[i] = new CalcolaValutazione(idStudente, percentualeAssenze, mediaVoti);
 			
 			//debug
-			System.out.println(idStudente + " " +  percentualeAssenze + " " + mediaVoti);
+			//System.out.println(idStudente + " " +  percentualeAssenze + " " + mediaVoti);
 			
 			boolean risultato = randomValutazione[i].promosso();
 			
@@ -44,17 +30,10 @@ public class Main {
 			}else {
 				System.out.println(idStudente + " " + "Bocciato");
 			}
-			
-			
-			
 
-			
 			} 
 
 
-		
 		}
-
-	
 
 }
